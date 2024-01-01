de445aca73232d14729e1366f034efc4a8d0fefa
hadil <hihijazi@gmail.com>
2023-12-31 19:19:13 -0500

    save changes

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import './App.css';
 import React from 'react';
 import Header from './Header.jsx';
 import Main from './Main.jsx'
+import PlaceReview from './PlaceReview.jsx';
 
 function App() {
   return (
-    <div className="App">
+    <div> className="App" 
+      <h1>Update Review</h1>
+      <PlaceReview placeId="1" reviewId="1" />
       <Header />
       <Main />
     </div>
--- src/PlaceReview.jsx
+++ src/PlaceReview.jsx
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { patchReview } from './api';
 
-function PlaceReview({ eachReview }) {
-  const { name, description, created } = eachReview;
+const PlaceReview = ({ placeId, reviewId }) => {
+ const [newReview, setNewReview] = useState({});
 
-  return (
-    <div className="horizontal-card-reviews">
-      <hr />
-      <small>{created}</small>
-      <p><small><b>{description}</b></small></p>
-      <p>{name}</p>
-    </div>
-  )
-}
+ const handleSubmit = async (e) => {
+    e.preventDefault();
+    await patchReview(placeId, reviewId, newReview);
+    // Perform any necessary actions after the review has been updated
+ };
+
+ const handleChange = (e) => {
+    setNewReview({ ...newReview, [e.target.name]: e.target.value });
+ };
+
+ return (
+    <form onSubmit={handleSubmit}>
+      <label>Name:</label>
+      <input type="text" name="name" onChange={handleChange} />
+
+      <label>Description:</label>
+      <textarea name="description" onChange={handleChange} />
+
+      <label>Created:</label>
+      <input type="text" name="created" onChange={handleChange} />
+
+      <button type="submit">Update Review</button>
+    </form>
+ );
+};
 
 export default PlaceReview;
