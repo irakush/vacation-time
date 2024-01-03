0b668b2e4ad8744d22ed7f4acf75294ccba9802b
hadil <hihijazi@gmail.com>
2024-01-02 19:44:17 -0500

    save changes

--- src/PlaceReview.jsx
+++ src/PlaceReview.jsx
@@ -4,14 +4,12 @@ import ReviewForm from './ReviewForm';
 function PlaceReview({ eachReview, onReviewSubmit }) {
   return (
     <div className="horizontal-card-reviews">
-      {/* Render existing reviews */}
       {eachReview.map((review) => (
         <div key={review.id}>
           <p>{review.name}</p>
           <p>{review.comment}</p>
         </div>
       ))}
-      {/* Render the review form */}
       <ReviewForm onSubmit={onReviewSubmit} />
     </div>
   );
